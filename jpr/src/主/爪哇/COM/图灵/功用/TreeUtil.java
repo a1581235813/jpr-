3b8829f137c8c9f03c2f55fb6df0fad9b942a026
caozhijun <1581235813@qq.com>
2019-12-05 10:05:55 +0800

    2

--- jpr/src/主/爪哇/COM/图灵/功用/TreeUtil.java
+++ jpr/src/主/爪哇/COM/图灵/功用/TreeUtil.java
@@ -1,49 +1,41 @@
 package com.turing.util;
 
 import java.util.ArrayList;
-
 import java.util.List;
 
 import com.turing.entity.SysMenus;
 
 public class TreeUtil {
-             public static List<SysMenus> findParent(List<SysMenus> list){
-            	 List<SysMenus> list2=new ArrayList<SysMenus>();
-            	 for (SysMenus menus : list) {
-					if(menus.getParentId()==1) {
-						
-							menus.setChildren(findSons(menus.getId(), list));
-							menus.setText(menus.getName());
-							
-							menus.setState("closed");
-							list2.add(menus);
-						
-						
-						
-							
-						
-					}
-				}
-				return list2;
-            	 
-             }
-	//这是数的插件
-             public static List<SysMenus> findSons (Long id,List<SysMenus> list){
-            	 List<SysMenus> list2=new ArrayList<SysMenus>();
-            	 for (SysMenus menus : list) {
-					if(menus.getParentId()==0) {
-						continue;
-					}
-					if(menus.getParentId()==id) {
-						menus.setChildren(findSons(menus.getId(), list));
-						
-						menus.setText(menus.getName());
-						
-					
-						list2.add(menus);
-					}
-				}
-				return list2;
-            	 
-             }
+	public static List<SysMenus> findParent(List<SysMenus> allList){
+		List<SysMenus> parents = new ArrayList<>();
+		for (SysMenus sysMenus : allList) {
+			if(sysMenus.getParentId()==1){
+				sysMenus.setText(sysMenus.getName());
+				//寻找一级菜单下的子节点
+				sysMenus.setChildren(findSons(sysMenus.getId(), allList));
+				parents.add(sysMenus);
+			}
+		}
+		return parents;
+	}
+	
+	//在所有菜单下 寻找指定父节点的子节点
+	public static List<SysMenus> findSons(long ParentId,List<SysMenus> allList){
+		List<SysMenus> sons = new ArrayList<>();
+		for(SysMenus menu:allList) {
+			if(menu.getParentId()== 1) {
+				continue;
+			}
+			if(menu.getParentId() == ParentId) {
+				//继续再找该菜单下子节点
+				//递归算法
+				menu.setText(menu.getName());
+				menu.setChildren(findSons(menu.getId(), allList));
+				//添加到集合
+				sons.add(menu);
+			}
+		}
+		return sons;
+	}
+	//测试
 }
